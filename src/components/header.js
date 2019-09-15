1b3ba5421773d100d20e6ed59e49412709a44098
github username <github email address>
2019-09-15 22:17:07 +0100

    header complete

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+import "./header.css"
+
+export default class Header extends Component
+{
+    render (){
+        return <div className="header">
+            <h2>To Do List</h2>
+        </div>
+    }
+}
---
+++ src/components/homepage.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Hompage extends Component
+{
+    state = {}
+
+    render (){
+        return <div>
+                <p>works</p>
+            </div>
+    }
+}
